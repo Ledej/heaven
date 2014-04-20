2ec2747d6746b2ebaca098bdca649958d9922194
Pierre Dulac <dulacpier@gmail.com>
2014-04-20 03:40:32 +0200

    Improve the unicorn configuration with resque

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,20 @@
 worker_processes 3
 timeout 30
+preload_app true
 
 @resque_pid = nil
 
 before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec rake resque:work QUEUES=*")
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info("Disconnected from Redis")
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(Resque)
+    Heaven.redis_reconnect!
+    Rails.logger.info("Connected to Redis")
+    @resque_pid ||= spawn("QUEUES=* bundle exec rake resque:work")
+  end
 end
